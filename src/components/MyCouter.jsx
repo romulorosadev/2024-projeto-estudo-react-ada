d4eb280917a5fcb1e2f200cf877b7283a430b513
Romulo Rosa <romulorosadev@gmail.com>
2024-07-07 15:59:38 -0300

    Alterando estado aumentar

--- src/components/MyCouter.jsx
+++ src/components/MyCouter.jsx
@@ -4,8 +4,13 @@ export default function MyCounter(){
 
   const [counter, setCounter] = useState(0);
 
+   function aumentar(){
+    setCounter(counter + 1);
+  }
+
   return(
     <div className="flex justify-center items-center gap-4">
+      <button onClick={aumentar} className="inline-block bg-white text-black font-bold p-4">aumentar</button>
 
       <h3 className=" text-white font-bold w-[10rem] text-center">CONTADOR: {counter}</h3>
 
