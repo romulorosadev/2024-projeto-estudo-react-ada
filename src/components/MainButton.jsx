8b6d1dffff8f90eb318080bf2d65503814b8431c
Romulo Rosa <romulorosadev@gmail.com>
2024-07-09 18:58:35 -0300

    deleted used files

--- src/components/MainButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function MainButton(props){
-  return(
-    <>
-      <button className="bg-white hover:bg-silver text-black rounded-[.5rem] px-[2rem] py-[.5rem] font-bold text-[1.2rem]">{props.cta}</button>
-    </>
-  )
-}
