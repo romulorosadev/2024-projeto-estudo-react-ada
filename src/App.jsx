387b01051bc72a28ea9b7df9527630f445a47792
Romulo Rosa <romulorosadev@gmail.com>
2024-07-09 18:58:56 -0300

    call myList

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from "react"
-import MainButtons from "./components/MainButtons"
-import MainCards from "./components/MainCards"
-import MainText from "./components/MainText"
 import MainTitle from "./components/MainTitle"
+import MyList from "./components/MyList"
 
 function App() {
   return (
     <div className="flex flex-col items-center bg-purple p-10 gap-[3rem] h-screen">
-      <MainTitle text="Rosadev Solutions" />
-      <MainText />
-      <MainCards />
-      <MainButtons />
+      <MainTitle text="Listas em React" />
+
+      <ul>
+        <MyList />
+      </ul>
     </div>
   )
 }
