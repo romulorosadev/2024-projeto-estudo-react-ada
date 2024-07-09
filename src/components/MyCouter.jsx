1897401add0ba99491a8ba4dab0aad59a63688fa
Romulo Rosa <romulorosadev@gmail.com>
2024-07-08 21:50:43 -0300

    carregamento condicional

--- src/components/MyCouter.jsx
+++ src/components/MyCouter.jsx
@@ -12,13 +12,33 @@ export default function MyCounter(){
     setCounter(counter - 1);
   }
 
+  if(counter > 5){
+    return (
+      <div className="flex flex-col gap-4">
+        <div>
+          {counter > 5 ? <p className="text-[20px] text-center font-bold">Valor Maior que 5</p> : null}
+        </div>
+
+        <div className="flex justify-center items-center gap-4">
+          <button onClick={diminuir} className="inline-block bg-black text-white font-bold p-4">diminuir</button>
+
+          <h3 className=" text-white font-bold w-[10rem] text-center">CONTADOR: {counter}</h3>
+
+          <button onClick={aumentar} className="inline-block bg-white text-black font-bold p-4">aumentar</button>
+        </div>
+    </div>
+    )
+  }
+
   return(
-    <div className="flex justify-center items-center gap-4">
-      <button onClick={aumentar} className="inline-block bg-white text-black font-bold p-4">aumentar</button>
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-center items-center gap-4">
+        <button onClick={diminuir} className="inline-block bg-black text-white font-bold p-4">diminuir</button>
 
-      <h3 className=" text-white font-bold w-[10rem] text-center">CONTADOR: {counter}</h3>
+        <h3 className=" text-white font-bold w-[10rem] text-center">CONTADOR: {counter}</h3>
 
-      <button onClick={diminuir} className="inline-block bg-black text-white font-bold p-4">diminuir</button>
+        <button onClick={aumentar} className="inline-block bg-white text-black font-bold p-4">aumentar</button>
+      </div>
     </div>
   )
 }
